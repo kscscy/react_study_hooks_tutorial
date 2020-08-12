ceed1602c14d5830a028726dc172a5ec50ce4c92
Charles Kang <kscscy@gmail.com>
2020-08-12 16:38:31 +0900

    hooks-tutorial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+import Info3 from "./Info3";
+
+// import Average4 from "./Average4";
+// import Average3 from "./Average3";
+// import Average2 from "./Average2";
+// import Average from "./Average";
+// import Info2 from "./Info2";
+// import Counter2 from "./Counter2";
+// import Info from "./Info";
+// import Counter from "./Counter";
+// import logo from './logo.svg';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {setVisible(!visible)}}
+//       >
+//         {visible ? '숨기기' : '보이기'}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return(
+//     <Counter2/>
+//   );
+// };
+
+// const App = () => {
+//   return(
+//     <Info2/>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <Average4/>
+//   );
+// };
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Info3/>
   );
-}
+};
 
 export default App;
---
+++ src/Info.jsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from "react";
+
+const Info = () => {
+  const [name, setName] = useState('');
+  const [nickname, setNickName] = useState('');
+  // useEffect(() => {
+  //   console.log('렌더링이 완료되었습니다!');
+  //   console.log({name, nickname});
+  // });
+  // useEffect(()=>{
+  //   console.log('마운트될 때만 실행됩니다.');
+  // }, []);
+  // useEffect(() => {
+  //   console.log('name',name);
+  // },[name]);
+
+  useEffect(()=>{
+    console.log('effect');
+    console.log(name);
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    }
+  });
+
+  const onChangeName = e => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = e => {
+    setNickName(e.target.value);
+  };
+
+  return(
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName}/>
+        <input value={nickname} onChange={onChangeNickname}/>
+      </div>
+      <div>
+        <div>
+          <b>이름: </b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
